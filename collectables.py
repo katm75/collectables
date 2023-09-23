6f200a0edc47996f98bef9252f2edbc4bbd81602
Kathryn <141536562+katm75@users.noreply.github.com>
2023-09-22 21:52:47 -0400

    Setting up inital file

--- collectables.py
+++ collectables.py
@@ -1,14 +1,31 @@
-	# Kathryn Marks
-	# kathryn.pythonprograms@gmail.com
-	# Created September 22, 2023
-	# Last modified September 22, 2023
+# Kathryn Marks
+# kathryn.pythonprograms@gmail.com
+# Created September 22, 2023
+# Last modified September 22, 2023
 
-You are interested in buying crypto-currencies. You want to check the current amount of money you have and see how many coins you can buy in Bitcoin, Ethereum, and Litecoin.
+# Excerise 5 from https://github.com/py-study-group/beginner-friendly-programming-exercises/blob/master/exercises.md
+#     Modified to change cryptocurrency to collectables
 
-Create a program that:
+# You are interested in buying crypto-currencies. 
+# You want to check the current amount of money you have and see how many coins you can buy in Bitcoin, Ethereum, and Litecoin.
 
-Reads the total amount of money you have
-Reads the price of Bitcoin, Ethereum, and Litecoin
-Prints the amount of Bitcoin, Ethereum, and Litecoin you can buy
-Example: money = 100, bitcoin_price = 50, ethereum_price = 25, litecoin_price = 10
-Output: "With 100$ you can buy: 2 Bitcoins, 4 Ethereum, and 10 Litecoins"
+# Create a program that:
+    # Reads the total amount of money you have
+    # Reads the price of Bitcoin, Ethereum, and Litecoin
+    # Prints the amount of Bitcoin, Ethereum, and Litecoin you can buy
+    # Example: money = 100, bitcoin_price = 50, ethereum_price = 25, litecoin_price = 10
+    # Output: "With 100$ you can buy: 2 Bitcoins, 4 Ethereum, and 10 Litecoins"
+
+# BEGIN PROGRAM
+
+# DEFINE VARIABLES
+    # money = the amount of money you have to spend in dollars
+    # dolls = the price of dolls
+    # books = the price of books
+    # stamps = the price of stamps
+
+
+
+
+
+#EOF
