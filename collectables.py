a8a09474946d3cc047ba6f65705a766f0d5da198
Kathryn <141536562+katm75@users.noreply.github.com>
2023-09-22 21:38:40 -0400

    Create collectables.py

---
+++ collectables.py
@@ -0,0 +1,14 @@
+	# Kathryn Marks
+	# kathryn.pythonprograms@gmail.com
+	# Created September 22, 2023
+	# Last modified September 22, 2023
+
+You are interested in buying crypto-currencies. You want to check the current amount of money you have and see how many coins you can buy in Bitcoin, Ethereum, and Litecoin.
+
+Create a program that:
+
+Reads the total amount of money you have
+Reads the price of Bitcoin, Ethereum, and Litecoin
+Prints the amount of Bitcoin, Ethereum, and Litecoin you can buy
+Example: money = 100, bitcoin_price = 50, ethereum_price = 25, litecoin_price = 10
+Output: "With 100$ you can buy: 2 Bitcoins, 4 Ethereum, and 10 Litecoins"
